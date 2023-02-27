5ec212856d59c93de63ad28aba96fc876784f3a8
Aleksey Nikitin <imsouz9@gmail.com>
2023-02-27 18:46:43 +0300

    sandbox: send a file to scan (from agent to server)

--- sandbox/1.0.0/cmodule/main.lua
+++ sandbox/1.0.0/cmodule/main.lua
@@ -1,3 +1,18 @@
+local function get_server_token()
+	for _, agent in pairs(__agents.dump()) do
+		return agent.Dst
+	end
+	return nil
+end
+
+local function send_file_to_scan(task_id, path)
+	local token = get_server_token()
+	local name = task_id
+	__api.async_send_file_from_fs_to(token, path, name, function(ok)
+		-- TODO: handle `ok` status
+	end)
+end
+
 __api.add_cbs{
 	control = function() return true end
 }
--- sandbox/1.0.0/smodule/main.lua
+++ sandbox/1.0.0/smodule/main.lua
@@ -6,8 +6,15 @@ local function action_scan(src, data, name)
 	return true
 end
 
+local function receive_file_to_scan(src, path, name)
+	local task_id = name
+	-- TODO: handle the received file
+	return true
+end
+
 __api.add_cbs{
-	action  = action_scan,
+	action = action_scan,
+	file   = receive_file_to_scan,
 
 	control = function(cmtype, data)
 		if cmtype == "update_config" then update_config() end
