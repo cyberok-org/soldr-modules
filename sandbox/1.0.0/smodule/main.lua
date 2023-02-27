165e00028ea43eb2557fc3e02157af876bda204a
Aleksey Nikitin <imsouz9@gmail.com>
2023-02-27 15:28:27 +0300

    sandbox: add action: cyberok_sandbox_scan

--- sandbox/1.0.0/smodule/main.lua
+++ sandbox/1.0.0/smodule/main.lua
@@ -1,5 +1,18 @@
+local function update_config()
+end
+update_config()
+
+local function action_scan(src, data, name)
+	return true
+end
+
 __api.add_cbs{
-	control = function() return true end
+	action  = action_scan,
+
+	control = function(cmtype, data)
+		if cmtype == "update_config" then update_config() end
+		return true
+	end,
 }
 __api.await(-1)
 return "success"
