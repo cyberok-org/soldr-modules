7ab519b71b40bac320c5457ea01b5a8c750f2941
Aleksey Nikitin <imsouz9@gmail.com>
2023-03-01 18:05:00 +0300

    sandbox: use Dispatcher to handle different callbacks

--- sandbox/1.0.0/cmodule/main.lua
+++ sandbox/1.0.0/cmodule/main.lua
@@ -1,26 +1,23 @@
-local cjson = require "cjson"
-local event = require "event"
+local dispatcher = require "dispatcher"
+local event      = require "event"
 
 local function update_config()
 	event.update_config()
 end
 update_config()
 
-local function handle_request_file(src, data)
+local handlers = dispatcher.by_type()
+
+function handlers.request_file(src, data)
 	local name = data.task_id
-	__api.async_send_file_from_fs_to(src, data.path, name, function(ok)
+	return __api.async_send_file_from_fs_to(src, data.path, name, function(ok)
 		event.error(string.format("send file: %s: failed", data.path))
 	end)
 end
 
 __api.add_cbs{
-	data = function(src, data)
-		data = cjson.decode(data)
-		if data.type == "request_file" then
-			return handle_request_file(src, data)
-		end
-	end,
-	control = function() return true end
+	data    = function(...) handlers(...) end,
+	control = function() return true end,
 }
 __api.await(-1)
 return "success"
