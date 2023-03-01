d2f2cf9b092c5e0c28c0acae57028c2be104e5d8
Aleksey Nikitin <imsouz9@gmail.com>
2023-03-01 15:35:27 +0300

    sandbox: Request a file from agent. Emit an event on error.

--- sandbox/1.0.0/cmodule/main.lua
+++ sandbox/1.0.0/cmodule/main.lua
@@ -1,19 +1,25 @@
-local function get_server_token()
-	for _, agent in pairs(__agents.dump()) do
-		return agent.Dst
-	end
-	return nil
-end
+local cjson = require "cjson"
+local event = require "event"
 
-local function send_file_to_scan(task_id, path)
-	local token = get_server_token()
-	local name = task_id
-	__api.async_send_file_from_fs_to(token, path, name, function(ok)
-		-- TODO: handle `ok` status
+local function update_config()
+	event.update_config()
+end
+update_config()
+
+local function handle_request_file(src, data)
+	local name = data.task_id
+	__api.async_send_file_from_fs_to(src, data.path, name, function(ok)
+		event.error(string.format("send file: %s: failed", data.path))
 	end)
 end
 
 __api.add_cbs{
+	data = function(src, data)
+		data = cjson.decode(data)
+		if data.type == "request_file" then
+			return handle_request_file(src, data)
+		end
+	end,
 	control = function() return true end
 }
 __api.await(-1)
--- sandbox/1.0.0/smodule/main.lua
+++ sandbox/1.0.0/smodule/main.lua
@@ -1,24 +1,57 @@
 local cjson = require("cjson.safe")
-local function update_config()
+
+--:: string -> string?
+function browser_to_agent_token(browser_token)
+	local browser = __agents.dump()[browser_token]
+	for _, info in pairs(__agents.get_by_id(browser.ID)) do
+		if tostring(info.Type) == "VXAgent" then
+			return info.Dst end
+	end
 end
-update_config()
+
+--:: string, string, string -> ok?
+local function request_file(agent_token, task_id, path)
+	return __api.send_data_to(agent_token, cjson.encode{
+		type    = "request_file",
+		task_id = task_id,
+		path    = path,
+	})
+end
+
+local function receive_file(src, path, name)
+	local task_id = name
+	-- TODO: handle the received file
+	return true
+end
 
 local function action_scan(src, data, name)
+	data = cjson.decode(data)
+	local path = data.data["object.fullpath"]
+
+	local agent_token = browser_to_agent_token(src)
+	if not agent_token then
+		__log.errorf("resolve destination to agent: failed")
+		return false
+	end
+
+	if not request_file(agent_token, "TODO_task_id", path) then
+		__log.errorf("request file path=%s: failed", path)
+		return false
+	end
+
 	local res = __api.send_data_to(src, cjson.encode({
 		type = "connection_error",
 	}))
 	return true
 end
 
-local function receive_file_to_scan(src, path, name)
-	local task_id = name
-	-- TODO: handle the received file
-	return true
+local function update_config()
 end
+update_config()
 
 __api.add_cbs{
 	action = action_scan,
-	file   = receive_file_to_scan,
+	file   = receive_file,
 
 	control = function(cmtype, data)
 		if cmtype == "update_config" then update_config() end
