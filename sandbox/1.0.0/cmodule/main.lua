9a8b37d5270b3adfee8b2790ba1f0efa5bfbfc73
Egor Artemov <egor.artemov@cyberok.ru>
2023-03-02 18:57:49 +0800

    Indent using spaces

--- sandbox/1.0.0/cmodule/main.lua
+++ sandbox/1.0.0/cmodule/main.lua
@@ -3,48 +3,49 @@ local event = require "event"
 local mmap  = require "mmap"
 
 local function get_server_token()
-	for _, agent in pairs(__agents.dump()) do
-		return agent.Dst
-	end
+    for _, agent in pairs(__agents.dump()) do
+        return agent.Dst
+    end
 end
 
 local function scan_file(server_token, path)
-	return __api.send_data_to(server_token, cjson.encode{
-		type = "scan_file",
-		path = path,
-	})
+    return __api.send_data_to(server_token, cjson.encode {
+        type = "scan_file",
+        path = path,
+    })
 end
 
 local handlers = mmap.new(function(src, data, name)
-	data = cjson.decode(data)
-	return name or data.type, src, data
+    data = cjson.decode(data)
+    return name or data.type, src, data
 end)
 
 -- Action: cyberok_sandbox_scan
 function handlers.cyberok_sandbox_scan(src, data)
-	return scan_file(get_server_token(), data.data["object.fullpath"])
+    return scan_file(get_server_token(), data.data["object.fullpath"])
 end
 
 function handlers.request_file(src, data)
-	local name = data.task_id
-	return __api.async_send_file_from_fs_to(src, data.path, name, function(ok)
-		if not ok then
-			event.error(string.format("send file: %s: failed", data.path)) end
-	end)
+    local name = data.task_id
+    return __api.async_send_file_from_fs_to(src, data.path, name, function(ok)
+        if not ok then
+            event.error(string.format("send file: %s: failed", data.path))
+        end
+    end)
 end
 
 local function update_config()
-	event.update_config()
+    event.update_config()
 end
 update_config()
 
-__api.add_cbs{
-	action  = handlers:as_function(),
-	data    = handlers:as_function(),
-	control = function(cmtype, data)
-		if cmtype == "update_config" then update_config() end
-		return true
-	end,
+__api.add_cbs {
+    action  = handlers:as_function(),
+    data    = handlers:as_function(),
+    control = function(cmtype, data)
+        if cmtype == "update_config" then update_config() end
+        return true
+    end,
 }
-__api.await(-1)
+__api.await( -1)
 return "success"
--- sandbox/1.0.0/smodule/main.lua
+++ sandbox/1.0.0/smodule/main.lua
@@ -3,62 +3,63 @@ local mmap  = require "mmap"
 
 --:: string -> string?
 function get_agent_token(src)
-	local node = __agents.dump()[src]
-	for _, info in pairs(__agents.get_by_id(node.ID)) do
-		if tostring(info.Type) == "VXAgent" then
-			return info.Dst end
-	end
+    local node = __agents.dump()[src]
+    for _, info in pairs(__agents.get_by_id(node.ID)) do
+        if tostring(info.Type) == "VXAgent" then
+            return info.Dst
+        end
+    end
 end
 
 --:: string, string, string -> ok?
 local function request_file(agent_token, task_id, path)
-	return __api.send_data_to(agent_token, cjson.encode{
-		type    = "request_file",
-		task_id = task_id,
-		path    = path,
-	})
+    return __api.send_data_to(agent_token, cjson.encode {
+        type    = "request_file",
+        task_id = task_id,
+        path    = path,
+    })
 end
 
 local function receive_file(src, path, name)
-	local task_id = name
-	-- TODO: handle the received file
-	return true
+    local task_id = name
+    -- TODO: handle the received file
+    return true
 end
 
 local handlers = mmap.new(function(src, data)
-	data = cjson.decode(data)
-	return data.type, src, data
+    data = cjson.decode(data)
+    return data.type, src, data
 end)
 
 function handlers.scan_file(src, data)
-	local agent_token = get_agent_token(src)
-	if not agent_token then
-		__log.errorf("resolve destination to agent: failed")
-		return false
-	end
+    local agent_token = get_agent_token(src)
+    if not agent_token then
+        __log.errorf("resolve destination to agent: failed")
+        return false
+    end
 
-	if not request_file(agent_token, "TODO_task_id", data.path) then
-		__log.errorf("request file path=%s: failed", data.path)
-		return false
-	end
+    if not request_file(agent_token, "TODO_task_id", data.path) then
+        __log.errorf("request file path=%s: failed", data.path)
+        return false
+    end
 
-	local res = __api.send_data_to(src, cjson.encode({
-		type = "connection_error",
-	}))
-	return true
+    local res = __api.send_data_to(src, cjson.encode({
+        type = "connection_error",
+    }))
+    return true
 end
 
 local function update_config()
 end
 update_config()
 
-__api.add_cbs{
-	data    = handlers:as_function(),
-	file    = receive_file,
-	control = function(cmtype, data)
-		if cmtype == "update_config" then update_config() end
-		return true
-	end,
+__api.add_cbs {
+    data    = handlers:as_function(),
+    file    = receive_file,
+    control = function(cmtype, data)
+        if cmtype == "update_config" then update_config() end
+        return true
+    end,
 }
-__api.await(-1)
+__api.await( -1)
 return "success"
