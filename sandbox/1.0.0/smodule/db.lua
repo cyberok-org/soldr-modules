d96cedc4e0863d37b68eda66dc43dfb0497d7402
Aleksey Nikitin <imsouz9@gmail.com>
2023-03-14 16:12:11 +0300

    sandbox: update comments

--- sandbox/1.0.0/smodule/db.lua
+++ sandbox/1.0.0/smodule/db.lua
@@ -15,6 +15,7 @@ CREATE TABLE IF NOT EXISTS scan (
 );
 ]]
 
+-- Apply migration onto the given database.
 --:: sqlite3.Db -> true?, error?
 local function migrate(db)
     local ok, err = try(function()
@@ -24,6 +25,7 @@ local function migrate(db)
     return ok, string.format("failed to apply migration: %s", err)
 end
 
+--:: sqlite3.Db, string, (sqlite3.Stmt -> ...) -> ...
 local function with_prepare(db, sql, func)
     return try(function()
         local stmt = db:prepare(sql)
@@ -36,6 +38,8 @@ end
 -- DB stores the state of scanning tasks.
 local DB = {}; DB.__index = DB
 
+-- Creates an instance of DB with openning an SQLite database from the file.
+-- A new database will be created unless the file exists.
 --:: string -> DB?, error?
 function DB.open(filename)
     return try(function()
@@ -45,6 +49,7 @@ function DB.open(filename)
     end)
 end
 
+-- Instructs DB to close the internal SQLite database connection.
 --:: () -> true?, error?
 function DB:close()
     return try(function()
@@ -59,6 +64,7 @@ function DB.datetime(now)
     return os.date("!%F %T", now)
 end
 
+-- Returns info about the requested scanning task.
 --:: ScanRow :: {scan_id, agent_id, path, status, ...}
 --:: string -> ScanRow?, error?
 function DB:scan_get(scan_id)
@@ -77,6 +83,7 @@ function DB:scan_get(scan_id)
     end)
 end
 
+-- Creates a new scanning task in the database.
 --:: string, string, integer? -> scan_id::string?, error?
 function DB:scan_new(agent_id, path, now)
     return with_prepare(self._db, [[
@@ -89,6 +96,7 @@ function DB:scan_new(agent_id, path, now)
     end)
 end
 
+-- Updates status of the scanning task: status=processing.
 --:: string, string, integer? -> boolean, error?
 function DB:scan_set_processing(scan_id, cuckoo_task_id, now)
     return with_prepare(self._db, [[
