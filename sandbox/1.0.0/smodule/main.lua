c55d58f11ca7f51f6264f0cfbc594389e81cf1fc
Aleksey Nikitin <imsouz9@gmail.com>
2023-03-15 15:17:13 +0300

    sandbox: store scanning task in a database

--- sandbox/1.0.0/smodule/main.lua
+++ sandbox/1.0.0/smodule/main.lua
@@ -1,12 +1,11 @@
 local cjson     = require "cjson"
 local courl     = require "courl"
+local Cuckoo    = require "cuckoo"
+local DB        = require "db"
 local go        = require "go"
 local MethodMap = require "mmap"
 local time      = require "time"
 local try       = require "try"
-local Cuckoo    = require "cuckoo"
-
-local cuckoo
 
 local function check(...)
     local ok, err = ...; if not ok then
@@ -14,6 +13,28 @@ local function check(...)
         __log.error(err)
     end; return ...
 end
+
+local db, err = check(DB.open(__gid..".cyberok_sandbox.db"))
+if err then
+    __api.await(-1)
+    return "success"
+end
+
+local cuckoo
+
+local function update_config()
+    -- todo: stop current connections
+    cuckoo = Cuckoo:new(
+        "http://192.168.220.236:8090",
+        "AWFKI9LcPk_Y5i0pcA6XKA"
+    )
+end
+update_config()
+
+local handlers = MethodMap.new(function(src, data)
+    data = cjson.decode(data)
+    return data.type, src, data
+end)
 
 --:: string, string? -> AgentInfo?, error?
 local function get_agent_by_src(src, type)
@@ -35,26 +56,35 @@ local function request_file(dst, scan_id, filename)
     })
 end
 
---:: string,string,string -> ok?
-local function receive_file(src, path, name)
-    go(function()
-        local task_id = check(cuckoo:create_task(path))
-    end)
-    return true
-end
-
-local handlers = MethodMap.new(function(src, data)
-    data = cjson.decode(data)
-    return data.type, src, data
-end)
-
 function handlers.scan_file(src, data)
     return check(try(function()
         local agent = assert(get_agent_by_src(src))
-        assert(request_file(agent.Dst, "TODO_scan_id", data.filename),
+        local scan_id, err = db:scan_new(agent.ID, data.filename),
+        assert(scan_id, string.format("creating a scanning task: %s", err))
+        assert(request_file(agent.Dst, scan_id, data.filename),
             string.format("request file %s: failed", data.filename))
         return true
     end))
+end
+
+--:: string, string, string -> ok?
+local function receive_file(src, path, name)
+    go(function()
+        check(try(function()
+            local scan_id = name
+            local scan, err = db:scan_get(scan_id)
+            assert(scan, string.format(
+                "getting the scanning task: scan_id=%s: %s", scan_id, err))
+
+            local task_id, err = cuckoo:create_task(path, scan.filename)
+            assert(task_id, string.format("cuckoo: creating a task: %s", err))
+
+            local ok, err = db:scan_set_processing(scan_id, task_id)
+            assert(ok, string.format("update the scanning task: %s", err))
+            return true
+        end))
+    end)
+    return true
 end
 
 function handlers.request_data(src, data)
@@ -67,15 +97,6 @@ function handlers.request_data(src, data)
         data = rows,
     })
 end
-
-local function update_config()
-    -- todo: stop current connections
-    cuckoo = Cuckoo:new(
-        "http://192.168.220.236:8090",
-        "AWFKI9LcPk_Y5i0pcA6XKA"
-    )
-end
-update_config()
 
 __api.add_cbs {
     data    = handlers:as_function(),
@@ -96,5 +117,7 @@ repeat
     local remains = deadline - time.clock()
     __api.await(math.max(1, remains*1000))
 until __api.is_close() and courl:idle()
+
+check(db:close())
 
 return "success"
