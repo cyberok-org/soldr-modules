94eda9b5b2736960989dd5bbad9e2b5155517dd4
Aleksey Nikitin <imsouz9@gmail.com>
2023-03-17 11:43:47 +0300

    sandbox: refactor tests in Error

--- sandbox/1.0.0/cmodule/error_spec.lua
+++ sandbox/1.0.0/cmodule/error_spec.lua
@@ -1,11 +1,15 @@
 local Error = require "error"
 
 describe("Error", function()
-	local err = Error("Error", "Error message")
+	local err = Error("Error", "%s message %d", "Test", 10)
+
+	it("should have the specified name", function()
+		assert.same("Error", err.name)
+	end)
 
 	it("sould be convertable to string", function()
-		assert.same("Error message", tostring(err))
-		assert.same("Error message", string.format("%s", err))
+		assert.same("Test message 10", tostring(err))
+		assert.same("Test message 10", string.format("%s", err))
 	end)
 
 	it("should use the name of surrounding function", function()
@@ -19,8 +23,10 @@ describe("Error", function()
 	end)
 
 	test("forward", function()
-		local err = err:forward("AgentID")
-		assert.same("AgentID", err.agent_id)
-		assert.same("Error message", tostring(err))
+		local err2 = err:forward("AgentID")
+		assert.is_nil(err.agent_id)
+		assert.same("AgentID", err2.agent_id)
+		assert.same(err.name, err2.name)
+		assert.same(tostring(err.name), tostring(err2.name))
 	end)
 end)
