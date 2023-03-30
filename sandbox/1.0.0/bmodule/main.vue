0984c07d39caa693efdff566544b39d1d0d5df2b
Aleksey Nikitin <imsouz9@gmail.com>
2023-03-30 17:55:56 +0300

    sandbox: update default SQL-query in browser

--- sandbox/1.0.0/bmodule/main.vue
+++ sandbox/1.0.0/bmodule/main.vue
@@ -73,7 +73,7 @@
     data: () => ({
       leftTab: undefined,
       filename: "",
-      sqlQuery: "SELECT scan_id, filename, status, report_url, error FROM scan ORDER BY scan_id DESC LIMIT 10;",
+      sqlQuery: "SELECT scan_id, updated_at, filename, status, report_url, error FROM scan ORDER BY updated_at DESC LIMIT 10;",
       results: undefined,
       maxTableHeight: 585,
       reportID: "",
