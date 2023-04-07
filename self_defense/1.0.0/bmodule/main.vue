4aca9b63f2078ec75ec74032bc859fb656823194
Egor Artemov <egor.artemov@cyberok.ru>
2023-04-07 15:05:06 +0800

    self-defense: create empty module from "basic" template

---
+++ self_defense/1.0.0/bmodule/main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <el-tabs tab-position="left" v-model="leftTab" />
+  </div>
+</template>
+
+<script>
+const name = "empty";
+
+module.exports = {
+  name,
+  props: ["protoAPI", "hash", "module", "api", "components", "viewMode"],
+  data: () => ({
+    leftTab: undefined,
+  }),
+};
+</script>
