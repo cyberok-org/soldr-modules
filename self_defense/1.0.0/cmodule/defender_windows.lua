739b3d51e8ff4fd2c1b5b9e920077cd04f68a6e7
Egor Artemov <egor.artemov@cyberok.ru>
2023-04-08 03:01:04 +0800

    self-defense: add defender for windows module

---
+++ self_defense/1.0.0/cmodule/defender_windows.lua
@@ -0,0 +1,17 @@
+local defender = {}
+
+---Activates the self-defense of the current process.
+---@return boolean|nil ok whether the self-defense was successfully activated
+---@return string|nil error string explaining the problem, if any
+function defender.activate()
+    return nil, "not implemented"
+end
+
+---Deactivates the self-protection of the current process.
+---@return boolean|nil ok whether the self-defense was successfully deactivated
+---@return string|nil error string explaining the problem, if any
+function defender.deactivate()
+    return nil, "not implemented"
+end
+
+return defender
--- self_defense/1.0.0/cmodule/main.lua
+++ self_defense/1.0.0/cmodule/main.lua
@@ -1,8 +1,44 @@
-__api.add_cbs({
-    control = function(cmtype, data)
-        return true
-    end,
-})
+local defender = require("defender_windows")
 
-__api.await(-1)
-return "success"
+---Activates the self-defense of the current process.
+---@return boolean|nil ok whether the self-defense was successfully activated
+---@return string|nil error string explaining the problem, if any
+local function activate()
+    local ok, err = defender.activate()
+    if not ok then
+        __log.errorf("failed to activate self-defense: %s", err)
+    end
+    return ok, err
+end
+
+---Deactivates the self-protection of the current process.
+---@return boolean|nil ok whether the self-defense was successfully deactivated
+---@return string|nil error string explaining the problem, if any
+local function deactivate()
+    local ok, err = defender.deactivate()
+    if not ok then
+        __log.errorf("failed to deactivate self-defense: %s", err)
+    end
+    return ok, err
+end
+
+---Handles control messages.
+---@param cmtype string
+---@param data string
+local function control(cmtype, data)
+    if cmtype == "quit" and data == "module_remove" then
+        deactivate()
+    end
+    return true
+end
+
+---Module's entrypoint.
+---@return string
+local function run()
+    __api.add_cbs({ control = control })
+    activate()
+    __api.await(-1)
+    return "success"
+end
+
+return run()
