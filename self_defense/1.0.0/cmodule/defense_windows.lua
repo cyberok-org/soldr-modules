b449428e726ceb77900527d4e3ef63421f625b73
Egor Artemov <egor.artemov@cyberok.ru>
2023-04-10 13:53:25 +0800

    self-defense: extract exploit_mitigation code into separate module

--- self_defense/1.0.0/cmodule/defense_windows.lua
+++ self_defense/1.0.0/cmodule/defense_windows.lua
@@ -1,105 +1,35 @@
-require("waffi.headers.windows")
-local lk32 = require("waffi.windows.kernel32")
-local ffi = require("ffi")
+local exploit_mitigation = require("exploit_mitigation")
 
 local defense = {}
 
---- Gets the last error as a string.
----@return string error
-local function get_last_error()
-    return tostring(lk32.GetLastError())
-end
-
-ffi.cdef([[
-typedef enum _PROCESS_MITIGATION_POLICY {
-    ProcessDEPPolicy,
-    ProcessASLRPolicy,
-    ProcessDynamicCodePolicy,
-    ProcessStrictHandleCheckPolicy,
-    ProcessSystemCallDisablePolicy,
-    ProcessMitigationOptionsMask,
-    ProcessExtensionPointDisablePolicy,
-    ProcessControlFlowGuardPolicy,
-    ProcessSignaturePolicy,
-    ProcessFontDisablePolicy,
-    ProcessImageLoadPolicy,
-    ProcessSystemCallFilterPolicy,
-    ProcessPayloadRestrictionPolicy,
-    ProcessChildProcessPolicy,
-    ProcessSideChannelIsolationPolicy,
-    ProcessUserShadowStackPolicy,
-    ProcessRedirectionTrustPolicy,
-    ProcessUserPointerAuthPolicy,
-    ProcessSEHOPPolicy,
-    MaxProcessMitigationPolicy
-} PROCESS_MITIGATION_POLICY, *PPROCESS_MITIGATION_POLICY;
-
-typedef struct _PROCESS_MITIGATION_DEP_POLICY {
-    union {
-        DWORD Flags;
-        struct {
-            DWORD Enable : 1;
-            DWORD DisableAtlThunkEmulation : 1;
-            DWORD ReservedFlags : 30;
-        };
-    };
-    BOOLEAN Permanent;
-} PROCESS_MITIGATION_DEP_POLICY, *PPROCESS_MITIGATION_DEP_POLICY;
-]])
-
-local mitigation_policies = {
-    data_execution_prevention = {
-        id = ffi.C.ProcessDEPPolicy,
-        schema = "PROCESS_MITIGATION_DEP_POLICY",
-    },
+defense.HARDENED_PROFILE = {
+    strategies = { "exploit_mitigation" },
+    exploit_mitigation = exploit_mitigation.HARDENED_PROFILE,
 }
 
-ffi.cdef([[
-BOOL SetProcessMitigationPolicy(PROCESS_MITIGATION_POLICY MitigationPolicy,
-                                PVOID lpBuffer, SIZE_T dwLength);
-
-BOOL GetProcessMitigationPolicy(HANDLE hProcess,
-                                PROCESS_MITIGATION_POLICY MitigationPolicy,
-                                PVOID lpBuffer, SIZE_T dwLength);
-]])
-
----Applies the `config` for the specified mitigation policy `name`
----and returns the previous configuration.
----@param name string name of the mitigation policy
----@param config table configuration to apply
----@return table|nil old configuration
----@return string|nil error string explaining the problem, if any
-function defense.mitigation(name, config)
-    local policy = mitigation_policies[name]
-    local buf = ffi.new(policy.schema)
-    local len = ffi.sizeof(policy.schema)
-    local process = lk32.GetCurrentProcess()
-    local ret = ffi.C.GetProcessMitigationPolicy(process, policy.id, buf, len)
-    if not ret then
-        return nil, get_last_error()
-    end
-    local old_config = {}
-    for key, value in pairs(config) do
-        old_config[key] = (buf[key] ~= 0)
-        buf[key] = value
-    end
-    ret = ffi.C.SetProcessMitigationPolicy(policy.id, buf, len)
-    if not ret then
-        return nil, get_last_error()
-    end
-    return old_config
-end
+local strategies = {
+    exploit_mitigation = exploit_mitigation,
+}
 
 ---Applies the `config` for the set of self-defense policies and returns
 ---the previous configuration.
 ---@return table|nil old configuration
 ---@return string|nil error string explaining the problem, if any
-function defense.apply()
-    return defense.mitigation("data_execution_prevention", {
-        Enable = 1,
-        Permanent = 1,
-        DisableAtlThunkEmulation = 1,
-    })
+function defense.apply(profile)
+    local old_profile = { strategies = {} }
+    for _, strategy_name in ipairs(profile.strategies) do
+        local strategy = strategies[strategy_name]
+        assert(strategy, string.format("unknown strategy %s", strategy_name))
+
+        local old_config, err = strategy.apply(profile[strategy_name])
+        if not old_config then
+            -- TODO: rollback applied strategies and return error
+            return nil, err
+        end
+        table.insert(old_profile.strategies, strategy_name)
+        old_profile[strategy_name] = old_config
+    end
+    return old_profile
 end
 
 return defense
--- self_defense/1.0.0/cmodule/defense_windows_spec.lua
+++ self_defense/1.0.0/cmodule/defense_windows_spec.lua
@@ -1,25 +1,20 @@
 local defense = require("defense_windows")
 
-describe("mitigation", function()
-    it("applies DEP policy config and returns the previous one", function()
-        local dep = defense.mitigation("data_execution_prevention", {
-            Enable = true,
-            Permanent = true,
-            DisableAtlThunkEmulation = true,
-        })
-        assert.is_table(dep)
-        assert.is_boolean(dep.Enable)
-        assert.is_boolean(dep.Permanent)
-        assert.is_boolean(dep.DisableAtlThunkEmulation)
-    end)
-end)
-
 describe("apply", function()
     it("applies self-defense config and returns the previous one ", function()
-        local old = defense.apply()
-        assert.is_table(old)
-        assert.is_not_nil(old["Enable"])
-        assert.is_not_nil(old["Permanent"])
-        assert.is_not_nil(old["DisableAtlThunkEmulation"])
+        local old_profile = defense.apply(defense.HARDENED_PROFILE)
+
+        assert.is_table(old_profile)
+        assert.same(defense.HARDENED_PROFILE.strategies, old_profile.strategies)
+
+        local exploit_mitigation = defense.HARDENED_PROFILE.exploit_mitigation
+        local old_exploit_mitigation = old_profile.exploit_mitigation
+        for policy_name, config in pairs(exploit_mitigation) do
+            local old_config = old_exploit_mitigation[policy_name]
+            assert.is_table(old_config)
+            for key, value in pairs(config) do
+                assert.equal(type(value), type(old_config[key]))
+            end
+        end
     end)
 end)
