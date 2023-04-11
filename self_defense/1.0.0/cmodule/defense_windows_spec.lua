b3cbb5594cf9798b20674420e8e3ddee3d30bf67
Egor Artemov <egor.artemov@cyberok.ru>
2023-04-12 05:33:44 +0800

    self-defense: remove unnecessary details from tests

--- self_defense/1.0.0/cmodule/defense_windows_spec.lua
+++ self_defense/1.0.0/cmodule/defense_windows_spec.lua
@@ -1,20 +1,24 @@
 local defense = require("defense_windows")
 
 describe("apply", function()
-    it("applies self-defense config and returns the previous one ", function()
-        local old_profile = defense.apply(defense.HARDENED_PROFILE)
+    it("applies strategies and returns previous profile on success", function()
+        local TEST_PROFILE = {
+            strategies = { "test_strategy" },
+            test_strategy = {
+                param = "test",
+            },
+        }
+        local test_strategy = {
+            apply = function()
+                return { param = "old" }
+            end,
+        }
+        local strategies = { test_strategy = test_strategy }
+
+        local old_profile = defense.apply(TEST_PROFILE, strategies)
 
         assert.is_table(old_profile)
-        assert.same(defense.HARDENED_PROFILE.strategies, old_profile.strategies)
-
-        local exploit_mitigation = defense.HARDENED_PROFILE.exploit_mitigation
-        local old_exploit_mitigation = old_profile.exploit_mitigation
-        for policy_name, config in pairs(exploit_mitigation) do
-            local old_config = old_exploit_mitigation[policy_name]
-            assert.is_table(old_config)
-            for key, value in pairs(config) do
-                assert.equal(type(value), type(old_config[key]))
-            end
-        end
+        assert.same(TEST_PROFILE.strategies, old_profile.strategies)
+        assert.same(test_strategy.apply(), old_profile.test_strategy)
     end)
 end)
--- self_defense/1.0.0/cmodule/main.lua
+++ self_defense/1.0.0/cmodule/main.lua
@@ -1,10 +1,96 @@
 local defense = require("defense_windows")
+local exploit_mitigation = require("exploit_mitigation")
+
+local HARDENED_PROFILE = {
+    strategies = { "exploit_mitigation" },
+    exploit_mitigation = {
+        data_execution_prevention = {
+            Enable = true,
+            Permanent = true,
+            DisableAtlThunkEmulation = true,
+        },
+        address_space_layout_randomization = {
+            EnableBottomUpRandomization = true,
+            EnableForceRelocateImages = true,
+            EnableHighEntropy = true,
+            DisallowStrippedImages = true,
+        },
+        dynamic_code = {
+            ProhibitDynamicCode = false, -- causes the lua to crash
+            AllowThreadOptOut = false,
+            AllowRemoteDowngrade = false,
+            AuditProhibitDynamicCode = true,
+        },
+        strict_handle_check = {
+            RaiseExceptionOnInvalidHandleReference = true,
+            HandleExceptionsPermanentlyEnabled = true,
+        },
+        system_call_disable = {
+            DisallowWin32kSystemCalls = false,
+            AuditDisallowWin32kSystemCalls = false,
+        },
+        extension_point_disable = {
+            DisableExtensionPoints = true,
+        },
+        control_flow_guard = {
+            EnableControlFlowGuard = false,
+            EnableExportSuppression = false,
+            StrictMode = false,
+            EnableXfg = false,
+            EnableXfgAuditMode = false,
+        },
+        binary_signature = {
+            MicrosoftSignedOnly = false,
+            StoreSignedOnly = false,
+            MitigationOptIn = false,
+            AuditMicrosoftSignedOnly = false,
+            AuditStoreSignedOnly = false,
+        },
+        font_disable = {
+            DisableNonSystemFonts = true,
+            AuditNonSystemFontLoading = true,
+        },
+        image_load = {
+            NoRemoteImages = true,
+            NoLowMandatoryLabelImages = true,
+            PreferSystem32Images = true,
+            AuditNoRemoteImages = true,
+            AuditNoLowMandatoryLabelImages = true,
+        },
+        redirection_trust = {
+            EnforceRedirectionTrust = true,
+            AuditRedirectionTrust = true,
+        },
+        side_channel_isolation = {
+            SmtBranchTargetIsolation = true,
+            IsolateSecurityDomain = true,
+            DisablePageCombine = true,
+            SpeculativeStoreBypassDisable = true,
+            RestrictCoreSharing = true,
+        },
+        user_shadow_stack = {
+            EnableUserShadowStack = true,
+            AuditUserShadowStack = true,
+            SetContextIpValidation = true,
+            AuditSetContextIpValidation = true,
+            EnableUserShadowStackStrictMode = true,
+            BlockNonCetBinaries = true,
+            BlockNonCetBinariesNonEhcont = true,
+            CetDynamicApisOutOfProcOnly = true,
+            SetContextIpValidationRelaxedMode = false,
+        },
+    },
+}
+
+local strategies = {
+    exploit_mitigation = exploit_mitigation,
+}
 
 ---Activates the self-defense of the current process.
 ---@return boolean|nil ok whether the self-defense was successfully activated
 ---@return string|nil error string explaining the problem, if any
 local function activate()
-    local old_profile, err = defense.apply(defense.HARDENED_PROFILE)
+    local old_profile, err = defense.apply(HARDENED_PROFILE, strategies)
     if not old_profile then
         __log.errorf("failed to activate self-defense: %s", err)
         return nil, err
