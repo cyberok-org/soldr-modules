9fe07dff2bfd58be0677ccce07cc6a71a9731ca8
Egor Artemov <egor.artemov@cyberok.ru>
2023-04-17 23:11:14 +0800

    self-defense: set permissions on object from SDDL string

--- self_defense/1.0.0/cmodule/security.lua
+++ self_defense/1.0.0/cmodule/security.lua
@@ -1,5 +1,5 @@
-local adv32 = require("waffi.windows.advapi32")
-local lk32 = require("waffi.windows.kernel32")
+local advapi32 = require("waffi.windows.advapi32")
+local kernel32 = require("waffi.windows.kernel32")
 local ffi = require("ffi")
 local script = require("script")
 
@@ -11,14 +11,15 @@ local security = {}
 ---@return number # size of the wide character string or 0 if conversion failed
 function security.utf8_to_wide_char(str)
     local ptr, size = ffi.cast("const char*", str), #str
-    local nsize = lk32.MultiByteToWideChar(lk32.CP_UTF8, 0, ptr, size, nil, 0)
+    local nsize = kernel32.MultiByteToWideChar(kernel32.CP_UTF8, 0, ptr, size, nil, 0)
 
     if nsize <= 0 then
         return nil, 0
     end
 
     local wstr = ffi.new("wchar_t[?]", nsize + 1)
-    lk32.MultiByteToWideChar(lk32.CP_UTF8, 0, ptr, size, wstr, nsize)
+
+    kernel32.MultiByteToWideChar(kernel32.CP_UTF8, 0, ptr, size, wstr, nsize)
 
     return wstr, nsize
 end
@@ -27,16 +28,17 @@ end
 ---@param str ffi.cdata* wide character string
 ---@param size number size of the wide character string
 ---@return string|nil # UTF-8 encoded string or nil if conversion failed
+---@return number # size of the wide character string or 0 if conversion failed
 function security.wide_char_to_utf8(str, size)
-    local nsize = lk32.WideCharToMultiByte(lk32.CP_UTF8, 0, str, size, nil, 0, nil, nil)
+    local nsize = kernel32.WideCharToMultiByte(kernel32.CP_UTF8, 0, str, size, nil, 0, nil, nil)
 
     if nsize <= 0 then
-        return nil
+        return nil, 0
     end
 
-    local utf8 = ffi.new("char[?]", nsize + 1)
-    lk32.WideCharToMultiByte(lk32.CP_UTF8, 0, str, size, utf8, nsize, nil, nil)
-    return ffi.string(utf8, nsize)
+    local utf8 = ffi.new("char[?]", nsize)
+    kernel32.WideCharToMultiByte(kernel32.CP_UTF8, 0, str, size, utf8, nsize, nil, nil)
+    return tostring(ffi.string(utf8, nsize)), nsize
 end
 
 ---@class SecurityDescriptor: Command
@@ -68,15 +70,39 @@ end
 ---@param err integer
 ---@return string # string representation of the error code
 local function winerror_tostring(err)
-    -- TODO: Get error string from code
+    local FORMAT_MESSAGE_FROM_SYSTEM = 0x00001000
+    local FORMAT_MESSAGE_IGNORE_INSERTS = 0x00000200
+    local FORMAT_MESSAGE_ALLOCATE_BUFFER = 0x00000100
+
+    local flags = FORMAT_MESSAGE_FROM_SYSTEM
+        + FORMAT_MESSAGE_IGNORE_INSERTS
+        + FORMAT_MESSAGE_ALLOCATE_BUFFER
+    local buffer = ffi.new("wchar_t*[1]")
+    local size = kernel32.FormatMessageW(flags, nil, err, 0, ffi.cast("LPWSTR", buffer), 0, nil)
+    if size == 0 then
+        return "unknown error"
+    end
+    local msg, _ = security.wide_char_to_utf8(buffer[0], size)
+    kernel32.LocalFree(buffer[0])
+    if msg then
+        return string.gsub(msg, "[\r\n]", "")
+    end
     return tostring(err)
 end
 
 ---Gets the last error as a string.
 ---@return string error
 local function get_last_error()
-    return winerror_tostring(lk32.GetLastError())
+    return winerror_tostring(kernel32.GetLastError())
 end
+
+local SDDL_REVISION_1 = 1
+local OWNER_SECURITY_INFORMATION = 0x00000001
+local GROUP_SECURITY_INFORMATION = 0x00000002
+local DACL_SECURITY_INFORMATION = 0x00000004
+local COMMON_SECURITY_INFO = OWNER_SECURITY_INFORMATION
+    + GROUP_SECURITY_INFORMATION
+    + DACL_SECURITY_INFORMATION
 
 ---@private
 ---Returns security descriptor string for the object with the specified `name` and `type`.
@@ -85,50 +111,48 @@ end
 ---@return string|nil # security descriptor string or nil if failed
 ---@return string|nil # error string or nil if succeeded
 function security.get_descriptor_string(name, type)
-    local OWNER_SECURITY_INFORMATION = 0x00000001
-    local GROUP_SECURITY_INFORMATION = 0x00000002
-    local DACL_SECURITY_INFORMATION = 0x00000004
-    local SECURITY_INFO_QUERY = OWNER_SECURITY_INFORMATION + GROUP_SECURITY_INFORMATION + DACL_SECURITY_INFORMATION
-    local object_name = security.utf8_to_wide_char(name)
+    local object_name, _ = security.utf8_to_wide_char(name)
     local psecurity_descriptor = ffi.new("PSECURITY_DESCRIPTOR_RELATIVE[1]")
 
-    local err = adv32.GetNamedSecurityInfoW(
+    local err = advapi32.GetNamedSecurityInfoW(
         object_name,
         type,
-        SECURITY_INFO_QUERY,
+        COMMON_SECURITY_INFO,
         nil, -- ppSidOwner
         nil, -- ppSidGroup
         nil, -- ppDacl,
         nil, -- ppSacl,
         psecurity_descriptor
     )
-    if err ~= lk32.ERROR_SUCCESS then
+    if err ~= kernel32.ERROR_SUCCESS then
         return nil, winerror_tostring(err)
     end
 
-    local SDDL_REVISION_1 = 1
     local pstring_security_descriptor = ffi.new("wchar_t*[1]")
     local pstring_security_descriptor_len = ffi.new("ULONG[1]")
-    local ok = adv32.ConvertSecurityDescriptorToStringSecurityDescriptorW(
+    local ok = advapi32.ConvertSecurityDescriptorToStringSecurityDescriptorW(
         ffi.cast("SECURITY_DESCRIPTOR *", psecurity_descriptor[0]),
         SDDL_REVISION_1,
-        SECURITY_INFO_QUERY,
+        COMMON_SECURITY_INFO,
         pstring_security_descriptor,
         pstring_security_descriptor_len
     )
     if ok == 0 then
-        lk32.LocalFree(psecurity_descriptor[0])
+        kernel32.LocalFree(psecurity_descriptor[0])
         return nil, get_last_error()
     end
-    lk32.LocalFree(psecurity_descriptor[0])
+    kernel32.LocalFree(psecurity_descriptor[0])
 
-    local undo_name = security.wide_char_to_utf8(pstring_security_descriptor[0], pstring_security_descriptor_len[0])
-    if not undo_name then
-        lk32.LocalFree(pstring_security_descriptor[0])
+    local descriptor_string, _ = security.wide_char_to_utf8(
+        pstring_security_descriptor[0],
+        pstring_security_descriptor_len[0]
+    )
+    if not descriptor_string then
+        kernel32.LocalFree(pstring_security_descriptor[0])
         return nil, "unable to convert security descriptor to utf-8"
     end
-    lk32.LocalFree(pstring_security_descriptor[0])
-    return undo_name
+    kernel32.LocalFree(pstring_security_descriptor[0])
+    return descriptor_string
 end
 
 ---Sets the security descriptor for the specified object, returning undo command.
@@ -137,11 +161,44 @@ function SecurityDescriptor:run()
     if not undo_descriptor then
         return nil, err
     end
+
+    local psecurity_descriptor = ffi.new("PSECURITY_DESCRIPTOR[1]")
+    local psecurity_descriptor_len = ffi.new("ULONG[1]")
+    local descriptor, _ = security.utf8_to_wide_char(self.descriptor)
+    local ok = advapi32.ConvertStringSecurityDescriptorToSecurityDescriptorW(
+        descriptor,
+        SDDL_REVISION_1,
+        psecurity_descriptor,
+        psecurity_descriptor_len
+    )
+    if ok == 0 then
+        return nil, get_last_error()
+    end
+
+    local object_name, _ = security.utf8_to_wide_char(self.name)
+    err = advapi32.SetNamedSecurityInfoW(
+        object_name,
+        self.type,
+        DACL_SECURITY_INFORMATION,
+        nil, -- psidOwner
+        nil, -- psidGroup
+        ffi.cast("PACL", psecurity_descriptor[0]), -- pDacl
+        nil -- pSacl
+    )
+    if err ~= kernel32.ERROR_SUCCESS then
+        return nil, winerror_tostring(err)
+    end
+    kernel32.LocalFree(psecurity_descriptor[0])
+
     return SecurityDescriptor:new(self.name, self.type, undo_descriptor)
 end
 
 function security.service_descriptor(name, descriptor)
-    return SecurityDescriptor:new(name, adv32.SE_SERVICE, descriptor)
+    return SecurityDescriptor:new(name, advapi32.SE_SERVICE, descriptor)
+end
+
+function security.file_descriptor(name, descriptor)
+    return SecurityDescriptor:new(name, advapi32.SE_FILE_OBJECT, descriptor)
 end
 
 return security
--- self_defense/1.0.0/cmodule/security_spec.lua
+++ self_defense/1.0.0/cmodule/security_spec.lua
@@ -1,25 +1,57 @@
 local ffi = require("ffi")
-local adv32 = require("waffi.windows.advapi32")
+local path = require("pl.path")
+local advapi32 = require("waffi.windows.advapi32")
 local security = require("security")
 
 describe("get_descriptor_string", function()
-    it("returns a security descriptor string for an existing file", function()
-        local file_name = "test_file.txt"
+    local file_name
+    before_each(function()
+        file_name = path.tmpname()
         io.open(file_name, "w"):close()
-
-        local dstring, err = security.get_descriptor_string(file_name, adv32.SE_FILE_OBJECT)
+    end)
+    after_each(function()
+        os.remove(file_name)
+    end)
+    it("returns a security descriptor string for an existing file", function()
+        local dstring, err = security.get_descriptor_string(file_name, advapi32.SE_FILE_OBJECT)
 
         assert.is_nil(err)
         assert.is_not_nil(dstring)
-        os.remove(file_name)
     end)
 
     it("returns an error for a non-existing file", function()
-        local file_name = "non_existing_file.txt"
+        local not_exist = "non_existing_file.txt"
 
-        local dstring, err = security.get_descriptor_string(file_name, adv32.SE_FILE_OBJECT)
+        local dstring, err = security.get_descriptor_string(not_exist, advapi32.SE_FILE_OBJECT)
 
         assert.is_nil(dstring)
         assert.is_not_nil(err)
+    end)
+end)
+
+describe("SecurityDescriptor:run", function()
+    local file_name
+    before_each(function()
+        file_name = path.tmpname()
+        io.open(file_name, "w"):close()
+    end)
+    after_each(function()
+        --os.remove(file_name)
+    end)
+    it("returns error if descriptor is invalid", function()
+        local undo, err = security.file_descriptor(file_name, "INVALID"):run()
+
+        assert.is_nil(undo)
+        assert.same("The parameter is incorrect.", err)
+    end)
+    it("sets security descriptor and returns undo", function()
+        local SYSTEM_ONLY =
+            "O:S-1-5-21-815770899-3706867064-1381326651-1001G:S-1-5-21-815770899-3706867064-1381326651-513D:PAI(A;;FA;;;SY)"
+
+        local undo, err = security.file_descriptor(file_name, SYSTEM_ONLY):run()
+
+        assert.is_nil(err)
+        assert.is_not_nil(undo)
+        print(file_name)
     end)
 end)
